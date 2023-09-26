2241bf68c2a5ffc9621ffb7071736445676d9af7
Mapie-llifr <misyma3d@aol.com>
2023-09-26 19:29:31 +0200

    mise a jour de requirements.txt

--- api.py
+++ api.py
@@ -11,10 +11,10 @@ api.config["DEBUG"] = False
 def hello_world():
     return "Hello World !"
 
-#MODEL = 'small_model_final.joblib'
+MODEL = 'small_model_final.joblib'
 #DATA_URL = "https://eu.pythonanywhere.com/user/Mapiellifr/files/home/Mapiellifr/OC_P7/small_df_model_final.csv"
 #MODEL_URL = "https://github.com/Mapie-llifr/OC_P7/blob/main/" + MODEL
-MODEL = "pipeline_lightGBM_final.joblib"        #local
+#MODEL = "pipeline_lightGBM_final.joblib"        #local
 DATA_URL = "./Docs_projet7/small_df_model_final.csv"  #local
 MODEL_URL = MODEL                               # local
 SEUIL = 0.20
