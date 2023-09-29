81a4d02d328be557bc91bccee0278b97eeec7e07
Mapie-llifr <misyma3d@aol.com>
2023-09-29 14:35:39 +0200

    documentation de api.py

--- api.py
+++ api.py
@@ -1,6 +1,7 @@
+# imports
+
 from flask import Flask
 from flask import request
-#import random
 import os
 import pandas as pd
 from joblib import load
@@ -8,10 +9,12 @@ from joblib import load
 api = Flask(__name__)
 api.config["DEBUG"] = False
 
+# Test pour vérifier le déploiement de l'API
 @api.route("/")
 def hello_world():
     return "Hello World !"
 
+# Liens vers les sources externes en usage local, ou après déploiement
 MODEL = 'small_model_final.joblib'
 #DATA_URL = "https://eu.pythonanywhere.com/user/Mapiellifr/files/home/Mapiellifr/OC_P7/small_df_model_final.csv"
 #MODEL_URL = "https://github.com/Mapie-llifr/OC_P7/blob/main/" + MODEL
@@ -22,12 +25,35 @@ SEUIL = 0.20
 
 
 def load_data(nrows):
+    """
+    Téléchargement du fichier externe de données
+
+    Parameters
+    ----------
+    nrows : int : nombre de ligne à télécharger.
+
+    Returns
+    -------
+    data : DataFrame.
+
+    """
     data = pd.read_csv(DATA_URL, nrows=nrows)
     data = data.drop('TARGET', axis=1)
     return data
 
 
 def prediction(client):
+    """
+    Retourne la prédiction du modèle pour un client.
+
+    Parameters
+    ----------
+    client : int : valeur de 'SK_ID_CURR' pour le client.
+
+    Returns
+    -------
+    y_pred : float : probabilité de non remboursement pour ce client.
+    """
     X_client = df[df['SK_ID_CURR'] == client]
     X_client = X_client.drop('SK_ID_CURR', axis=1)
     y_pred = clf.predict_proba(X_client)[0,1]
@@ -35,6 +61,18 @@ def prediction(client):
 
 
 def accord(pred) : 
+    """
+    definit l'interprétation à faire de la prédiction en fonction du SEUIL. 
+
+    Parameters
+    ----------
+    pred : float : probabilité de non remboursement.
+
+    Returns
+    -------
+    pret : int : 1, pret aaccordé ; 5, pret risqué ; 0, pret refusé.
+
+    """
     if pred < SEUIL :
         pret = 1
     elif pred < 0.5 : 
@@ -45,7 +83,18 @@ def accord(pred) :
 
 
 def make_feats(client):
-    
+    """
+    Retourne un dictionnaire des features les plus importantes localement dans la prédiction. 
+
+    Parameters
+    ----------
+    client : int : valeur de 'SK_ID_CURR' pour le client.
+
+    Returns
+    -------
+    feature_importance : dict : clef, nom de la variable ; value, importances dans la prédiction.
+
+    """
     X_client = df[df['SK_ID_CURR'] == client]
     X_client = X_client.drop('SK_ID_CURR', axis=1)
     
@@ -70,6 +119,7 @@ df = load_data(3000)
 
 clf = load(MODEL_URL)
 
+# Fournit sur requete la probabilité de non remboursement et l'interprétation à en faire. 
 @api.route("/predict")
 def predict():
     if 'id' in request.args:
@@ -82,6 +132,7 @@ def predict():
             'score': score_metier, 
             'id' : id_client}
 
+# Fournit sur requete la feature importance locale pour les dix meilleurs features. 
 @api.route("/feats")
 def feat_import():
     clf = load(MODEL_URL)
@@ -92,7 +143,7 @@ def feat_import():
     feature_importance = make_feats(id_client)
     return feature_importance
 
-#api.run()
+#api.run()       #local
 
 port = int(os.environ.get("PORT", 5000))
 api.run(host='0.0.0.0', port=port)
